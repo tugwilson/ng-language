26986f61d24fc2069b6acc49598c58f80c3759dc
tugwilson <tugwilson@eec585a2-2a2c-0410-a496-1f0d2ca5a3d9>
2007-03-21 15:13:25 +0000

    Add the reverse operators to InstanceHandler

--- ng/src/ng/runtime/InstanceHandler.java
+++ ng/src/ng/runtime/InstanceHandler.java
@@ -107,4 +107,221 @@ public interface InstanceHandler extends MetaClass {
    * @return
    */
   Object call(Object instance, Object p1, Object p2, Object p3, Object p4);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseAdd(Object operand, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseAddEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseSubtract(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseSubtractEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseMultiply(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseMultiplyEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseDivide(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseDivideEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseRemainder(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseRemainderEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseIntegerDivide(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseIntegerDivideEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseAnd(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseAndEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseOr(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseOrEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseXor(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseXorEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseLeftShift(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseLeftShiftEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseArithmeticRightShift(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseArithmeticRightShiftEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseLogicalRightShift(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseLogicalRightShiftEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseCompare(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseNotEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseLessThan(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseGreaterThan(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseLessThanOrEquals(Object lhs, Object rhs);
+  
+  /**
+   * @param lhs
+   * @param rhs
+   * @return
+   */
+  Object reverseGreaterThanOrEquals(Object lhs, Object rhs);
 }
