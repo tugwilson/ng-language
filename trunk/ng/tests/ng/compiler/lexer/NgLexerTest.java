8c84bc182b7fe4fdf71ef65a010040242d3c5279
tugwilson <tugwilson@eec585a2-2a2c-0410-a496-1f0d2ca5a3d9>
2007-04-09 08:45:56 +0000

    Fix bug with keyword recognition

--- trunk/ng/tests/ng/compiler/lexer/NgLexerTest.java
+++ trunk/ng/tests/ng/compiler/lexer/NgLexerTest.java
@@ -30,31 +30,57 @@ import junit.framework.TestCase;
  *
  */
 public class NgLexerTest extends TestCase {
-  public void testOperatorRecognition() throws IOException {
-  final NgLexer lexer = new NgLexer(new StringReader("+ ++ += - -- -= / /= % %= ÷ ÷= * *= ** **= & &= && | |= || ^ ^= >> >>= >>> >>>= << <<= = == != > >= < <= <=> ! ~"));
-  final Class[] expected = new Class[] {
-      PlusToken.class, IncrementToken.class, PlusEqualsToken.class,
-      MinusToken.class, DecrementToken.class, MinusEqualsToken.class,
-      DivideToken.class, DivideEqualsToken.class,
-      ModuloToken.class, ModuloEqualsToken.class,
-      IntDivToken.class, IntDivEqualsToken.class,
-      MultiplyToken.class, MultiplyEqualsToken.class, PowerToken.class, PowerEqualsToken.class,
-      ArithmeticAndToken.class, ArithmeticAndEqualsToken.class, BooleanAndToken.class,
-      ArithmeticOrToken.class, ArithmeticOrEqualsToken.class, BooleanOrToken.class,
-      ArithmeticXorToken.class, ArithmeticXorEqualsToken.class,
-      RightShiftToken.class, RightShiftEqualsToken.class, LogicalRightShiftToken.class, LogicalRightShiftEqualsToken.class,
-      LeftShiftToken.class, LeftShiftEqualsToken.class,
-      EqualsToken.class, EqualsEqualsToken.class, NotEqualsToken.class,
-      GreaterThanToken.class, GreaterThanOrEqualsToken.class,
-      LessThanToken.class, LessThanOrEqualsToken.class,
-      CompareToken.class,
-      NotToken.class,
-      ComplementToken.class,
-      EOFToken.class
-  };
-  
-    for (int i = 0; i != expected.length; i++) {
-      assertEquals(expected[i], lexer.nextToken().getClass());
+  public void testSymbolRecognition() throws IOException {
+    final NgLexer lexer = new NgLexer(new StringReader("+ ++ += - -- -= / /= % %= ÷ ÷= * *= ** **= & &= && | |= || ^ ^= >> >>= >>> >>>= << <<= = == != > >= < <= <=> ! ~,[](){}"));
+    final Class[] expected = new Class[] {
+        PlusToken.class, IncrementToken.class, PlusEqualsToken.class,
+        MinusToken.class, DecrementToken.class, MinusEqualsToken.class,
+        DivideToken.class, DivideEqualsToken.class,
+        ModuloToken.class, ModuloEqualsToken.class,
+        IntDivToken.class, IntDivEqualsToken.class,
+        MultiplyToken.class, MultiplyEqualsToken.class, PowerToken.class, PowerEqualsToken.class,
+        ArithmeticAndToken.class, ArithmeticAndEqualsToken.class, BooleanAndToken.class,
+        ArithmeticOrToken.class, ArithmeticOrEqualsToken.class, BooleanOrToken.class,
+        ArithmeticXorToken.class, ArithmeticXorEqualsToken.class,
+        RightShiftToken.class, RightShiftEqualsToken.class, LogicalRightShiftToken.class, LogicalRightShiftEqualsToken.class,
+        LeftShiftToken.class, LeftShiftEqualsToken.class,
+        EqualsToken.class, EqualsEqualsToken.class, NotEqualsToken.class,
+        GreaterThanToken.class, GreaterThanOrEqualsToken.class,
+        LessThanToken.class, LessThanOrEqualsToken.class,
+        CompareToken.class,
+        NotToken.class,
+        ComplementToken.class,
+        CommaToken.class,
+        OpenSquareBracketToken.class, CloseSquareBracketToken.class,
+        OpenRoundBracketToken.class, CloseRoundBracketToken.class,
+        OpenCurlyBracketToken.class, CloseCurlyBracketToken.class,      
+        EOFToken.class
+    };
+    
+      for (int i = 0; i != expected.length; i++) {
+        assertEquals(expected[i], lexer.nextToken().getClass());
+      }
     }
-  }
+  public void testKeywordRecognition() throws IOException {
+    final NgLexer lexer = new NgLexer(new StringReader(
+        "abstract abstract1 assert assert1 any any1 as as1 " +
+        "" +
+        "" +
+        "" +
+        "" +
+        "" +
+        "" +
+        "" +
+        "" +
+        ""
+        ));
+    final Class[] expected = new Class[] {
+        AbstractToken.class, IdentifierToken.class, AssertToken.class, IdentifierToken.class, AnyToken.class, IdentifierToken.class, AsToken.class, IdentifierToken.class, 
+        EOFToken.class
+    };
+    
+      for (int i = 0; i != expected.length; i++) {
+        assertEquals(expected[i], lexer.nextToken().getClass());
+      }
+    }
 }
