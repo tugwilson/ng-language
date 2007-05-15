9f1bf58046da7fd001ca1a38bee5a26b8d154135
tugwilson <tugwilson@eec585a2-2a2c-0410-a496-1f0d2ca5a3d9>
2007-05-15 09:37:05 +0000

    use java.bean.Introspectior to find properties rather than looking at method names.

--- ng/src/uk/co/wilson/ng/runtime/metaclass/reflection/InstanceReflectionHandler.java
+++ ng/src/uk/co/wilson/ng/runtime/metaclass/reflection/InstanceReflectionHandler.java
@@ -1,9 +1,15 @@
 package uk.co.wilson.ng.runtime.metaclass.reflection;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.MethodDescriptor;
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import ng.lang.NgRuntimeException;
 import ng.runtime.MetaMethod;
 import uk.co.wilson.ng.runtime.metaclass.InstanceHandlerImpl;
 import uk.co.wilson.ng.runtime.metaclass.reflection.fields.*;
@@ -31,61 +37,16 @@ public class InstanceReflectionHandler extends InstanceHandlerImpl {
      final Method method = methods[i];
      
        if (method.getDeclaringClass() == theClass) {
-       final Class returnType = method.getReturnType();
-       final MetaMethod metaMethod;
-       
-         if(returnType == boolean.class) {
-           metaMethod = new BooleanMetaMethod(method);
-         } else if (returnType == char.class) {
-           metaMethod = new CharMetaMethod(method);
-         } else if (returnType == byte.class) {
-           metaMethod = new ByteMetaMethod(method);
-         } else if (returnType == short.class) {
-           metaMethod = new ShortMetaMethod(method);
-         } else if (returnType == int.class) {
-           metaMethod = new IntMetaMethod(method);
-         } else if (returnType == long.class) {
-           metaMethod = new LongMetaMethod(method);
-         } else if (returnType == float.class) {
-           metaMethod = new FloatMetaMethod(method);
-         } else if (returnType == double.class) {
-           metaMethod = new DoubleMetaMethod(method);
-         } else if (returnType == void.class) {
-           metaMethod = new VoidMetaMethod(method);
-         } else if (returnType == Object.class) {
-           metaMethod = new UntypedMetaMethod(method);
-         } else {
-           metaMethod = new TypedMetaMethod(method);
-         }
+       final MetaMethod metaMethod = createMetaMethod(method);
          
          switch(method.getParameterTypes().length) {
            case 0: {
-           final String name = method.getName();
-           
-             this.zeroParameterMethods.put(name, metaMethod);
-             
-             if (name.startsWith("get") && name.length() > 3) {
-               this.getPropertyMethods.put(name.substring(3, 4).toLowerCase() + name.substring(4), metaMethod);
-             } else if (name.startsWith("is") && name.length() > 2 && method.getReturnType() == boolean.class) {
-               this.getPropertyMethods.put(name.substring(2, 3).toLowerCase() + name.substring(3), metaMethod);
-             }
-             
+             this.zeroParameterMethods.put(method.getName(), metaMethod);
              break;
            }
            
            case 1: {
-           final String name = method.getName();
-             
              this.oneParameterMethods.put(method.getName(), metaMethod);
-             
-             if (name.startsWith("set") && name.length() > 3) {
-               if (returnType == void.class) {
-                 this.setPropertyMethods.put(name.substring(3, 4).toLowerCase() + name.substring(4), new VoidSetPropertyMetaMethod(method));
-               } else {
-                 this.setPropertyMethods.put(name.substring(3, 4).toLowerCase() + name.substring(4), metaMethod);
-               }
-             }
-             
              break;
            }
            
@@ -105,6 +66,27 @@ public class InstanceReflectionHandler extends InstanceHandlerImpl {
              this.multiParameterMethods.put(method.getName(), metaMethod);
          }
        }
+     }
+     
+     try {
+     final BeanInfo beanInfo = Introspector.getBeanInfo(theClass);
+     final PropertyDescriptor propertyDescriptors[] = beanInfo.getPropertyDescriptors();
+     
+       for (int i = 0; i!= propertyDescriptors.length; i++) {
+         final Method readMethod = propertyDescriptors[i].getReadMethod();
+         final Method writeMethod = propertyDescriptors[i].getWriteMethod();
+         
+         if (readMethod != null && readMethod.getDeclaringClass() == theClass) {
+           this.getPropertyMethods.put(readMethod.getName(), createMetaMethod(readMethod));
+         }
+         
+         if (writeMethod != null && writeMethod.getDeclaringClass() == theClass) {
+           this.getPropertyMethods.put(writeMethod.getName(), createMetaMethod(writeMethod));
+         }
+       }
+     
+     } catch (final IntrospectionException e) {
+      throw new NgRuntimeException(e);
      }
      
      for (int i = 0; i != fields.length; i++) {
@@ -145,6 +127,38 @@ public class InstanceReflectionHandler extends InstanceHandlerImpl {
            this.setFieldMethods.put(field.getName(), new SetTypedFieldMetaMethod(field));
          }
        }
+     }
+  }
+
+  /**
+   * @param method
+   * @return
+   */
+  private MetaMethod createMetaMethod(final Method method) {
+  final Class returnType = method.getReturnType();
+
+     if(returnType == boolean.class) {
+       return new BooleanMetaMethod(method);
+     } else if (returnType == char.class) {
+       return new CharMetaMethod(method);
+     } else if (returnType == byte.class) {
+       return new ByteMetaMethod(method);
+     } else if (returnType == short.class) {
+       return new ShortMetaMethod(method);
+     } else if (returnType == int.class) {
+       return new IntMetaMethod(method);
+     } else if (returnType == long.class) {
+       return new LongMetaMethod(method);
+     } else if (returnType == float.class) {
+       return new FloatMetaMethod(method);
+     } else if (returnType == double.class) {
+       return new DoubleMetaMethod(method);
+     } else if (returnType == void.class) {
+       return new VoidMetaMethod(method);
+     } else if (returnType == Object.class) {
+       return new UntypedMetaMethod(method);
+     } else {
+       return new TypedMetaMethod(method);
      }
   }
 }
