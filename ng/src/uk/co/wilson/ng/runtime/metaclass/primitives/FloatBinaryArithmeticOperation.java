8d2cf1bcf8cc49bb1eb1b1b01f78f3fd0d1878ec
tugwilson <tugwilson@eec585a2-2a2c-0410-a496-1f0d2ca5a3d9>
2008-01-20 11:57:43 +0000

    Add missing exceptions

--- ng/src/uk/co/wilson/ng/runtime/metaclass/primitives/FloatBinaryArithmeticOperation.java
+++ ng/src/uk/co/wilson/ng/runtime/metaclass/primitives/FloatBinaryArithmeticOperation.java
@@ -4,16 +4,17 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import ng.runtime.metaclass.BinaryArithmeticOperation;
+import ng.runtime.threadcontext.NotPerformed;
 import ng.runtime.threadcontext.ThreadContext;
 
 public interface FloatBinaryArithmeticOperation extends BinaryArithmeticOperation, FloatBinaryOperation {
-  float doFloatApply(ThreadContext tc, float lhs, char rhs);
-  float doFloatApply(ThreadContext tc, float lhs, byte rhs);
-  float doFloatApply(ThreadContext tc, float lhs, short rhs);
-  float doFloatApply(ThreadContext tc, float lhs, int rhs);
-  float doFloatApply(ThreadContext tc, float lhs, long rhs);
-  float doFloatApply(ThreadContext tc, float lhs, float rhs);
-  double doDoubleApply(ThreadContext tc, float lhs, double rhs);
-  float doFloatApply(ThreadContext tc, float lhs, BigInteger rhs);
-  BigDecimal doBigDecimalApply(ThreadContext tc, float lhs, BigDecimal rhs);
+  float doFloatApply(ThreadContext tc, float lhs, char rhs) throws NotPerformed;
+  float doFloatApply(ThreadContext tc, float lhs, byte rhs) throws NotPerformed;
+  float doFloatApply(ThreadContext tc, float lhs, short rhs) throws NotPerformed;
+  float doFloatApply(ThreadContext tc, float lhs, int rhs) throws NotPerformed;
+  float doFloatApply(ThreadContext tc, float lhs, long rhs) throws NotPerformed;
+  float doFloatApply(ThreadContext tc, float lhs, float rhs) throws NotPerformed;
+  double doDoubleApply(ThreadContext tc, float lhs, double rhs) throws NotPerformed;
+  float doFloatApply(ThreadContext tc, float lhs, BigInteger rhs) throws NotPerformed;
+  BigDecimal doBigDecimalApply(ThreadContext tc, float lhs, BigDecimal rhs) throws NotPerformed;
 }
