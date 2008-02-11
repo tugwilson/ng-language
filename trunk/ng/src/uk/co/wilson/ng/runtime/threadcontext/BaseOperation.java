a59a6eb6727d8326b25a291600d2ff07c6b734cb
tugwilson <tugwilson@eec585a2-2a2c-0410-a496-1f0d2ca5a3d9>
2008-02-11 07:47:14 +0000

    Start Category support implementation

--- trunk/ng/src/uk/co/wilson/ng/runtime/threadcontext/BaseOperation.java
+++ trunk/ng/src/uk/co/wilson/ng/runtime/threadcontext/BaseOperation.java
@@ -1,8 +1,9 @@
 package uk.co.wilson.ng.runtime.threadcontext;
 
+import ng.runtime.threadcontext.Operation;
 import ng.runtime.threadcontext.ThreadContext;
 
-public class BaseOperation {
+public abstract class BaseOperation implements Operation {
   protected final ThreadContext tc;
 
   public BaseOperation(final ThreadContext tc) {
