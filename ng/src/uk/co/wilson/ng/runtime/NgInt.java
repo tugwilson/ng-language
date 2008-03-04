1ec349cc1442045c726404b2b2438c7776003808
tugwilson <tugwilson@eec585a2-2a2c-0410-a496-1f0d2ca5a3d9>
2008-03-04 08:04:28 +0000

    add some primitive container pre allocation

--- ng/src/uk/co/wilson/ng/runtime/NgInt.java
+++ ng/src/uk/co/wilson/ng/runtime/NgInt.java
@@ -7,12 +7,21 @@ import ng.runtime.metaclass.util.PrimitiveMetaClasses;
 
 public class NgInt extends BaseNgObject {
   private static final IntMetaClass metaclass = PrimitiveMetaClasses.getIntMetaClass();
+  private static final int range = 127;
+  private static final NgInt cache[] = new NgInt[range * 2 + 1];
+  static {
+    for (int i = 0; i != cache.length; i++) {
+      cache[i] = new NgInt(i - range);
+    }
+  }
 
   public static IntMetaClass get$MetaClass() {
     return metaclass;
   }
 
   public static NgInt valueOf(final int value) {
+    if (value >= -range && value <= range) return cache[value + range];
+    
     return new NgInt(value);
   }
 
